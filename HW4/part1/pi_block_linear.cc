22796e8004bc790d5d8c0c28b985f8ac70e70c47
CTFang's Macbook <andy89923@gmail.com>
2021-11-30 00:42:11 +0800

    PI Linear

--- HW4/part1/pi_block_linear.cc
+++ HW4/part1/pi_block_linear.cc
@@ -5,8 +5,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     // --- DON'T TOUCH ---
     MPI_Init(&argc, &argv);
     double start_time = MPI_Wtime();
@@ -16,19 +15,60 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: init MPI
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    if (world_rank > 0)
-    {
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+
+    if (world_rank > 0) {
         // TODO: handle workers
+        int num_toss;
+        MPI_Recv(&num_toss, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        
+        int seed = world_rank ^ 31415926;
+        srand(seed);
+        
+        int* ans = calloc(1, sizeof(int));
+
+        double rand_min = -1.0;
+        double rand_max =  1.0;
+        double x, y;
+        for (int i = 0; i < num_toss; i++) {
+            x = (rand_max - rand_min) * rand() / (RAND_MAX + 1.0) + rand_min;
+            y = (rand_max - rand_min) * rand() / (RAND_MAX + 1.0) + rand_min;
+
+            if (x * x + y * y - rand_max <= eps) *ans += 1;
+        }
+
+        MPI_Send(ans, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
-    else if (world_rank == 0)
-    {
+    else if (world_rank == 0) {
         // TODO: master
+
+        int num_toss = tosses / (world_size - 1);
+        int* trows = calloc(world_size, sizeof(int));
+
+        for (int i = 1; i < world_size; i++) {
+            trows[i] = num_toss;
+
+            if (i == world_size - 1)
+                trows[i] += tosses % (world_size - 1);
+
+            MPI_Send(&trows[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
     }
 
-    if (world_rank == 0)
-    {
+    if (world_rank == 0) {
         // TODO: process PI result
+        int ans = 0, tmp;
+
+        for (int i = 1; i < world_size; i++) {
+            MPI_Recv(&tmp, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            ans += tmp;
+        }
+        pi_result = (double) ans / tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
