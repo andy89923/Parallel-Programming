b48d8b0057323b03aa53801ac0b818916b57fd34
CTFang's PP workstation <ctfang.cs08@nycu.edu.tw>
2021-11-29 17:23:29 +0000

    HW4 part1 linear

--- HW4/part1/pi_block_linear.cc
+++ HW4/part1/pi_block_linear.cc
@@ -4,6 +4,9 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#define eps 1e-9
+
+#pragma GCC optimize("Ofast", "unroll-loops")
 
 int main(int argc, char **argv) {
     // --- DON'T TOUCH ---
@@ -15,60 +18,69 @@ int main(int argc, char **argv) {
     // ---
 
     // TODO: init MPI
-    int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
+	
+	double rand_min = -1.0;
+	double rand_max =  1.0;
+	int ans_sum = 0;
 
     if (world_rank > 0) {
         // TODO: handle workers
         int num_toss;
         MPI_Recv(&num_toss, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         
-        int seed = world_rank ^ 31415926;
-        srand(seed);
-        
-        int* ans = calloc(1, sizeof(int));
+        unsigned int seed = world_rank ^ time(NULL);
+        int* ans = (int*) calloc(1, sizeof(int));
 
-        double rand_min = -1.0;
-        double rand_max =  1.0;
         double x, y;
         for (int i = 0; i < num_toss; i++) {
-            x = (rand_max - rand_min) * rand() / (RAND_MAX + 1.0) + rand_min;
-            y = (rand_max - rand_min) * rand() / (RAND_MAX + 1.0) + rand_min;
+            x = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
+            y = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
 
             if (x * x + y * y - rand_max <= eps) *ans += 1;
         }
+		// printf("%d %d %d\n", world_rank, num_toss, *ans);
 
         MPI_Send(ans, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
     else if (world_rank == 0) {
         // TODO: master
 
-        int num_toss = tosses / (world_size - 1);
-        int* trows = calloc(world_size, sizeof(int));
-
+        int num_toss = tosses / world_size;
+        int* trows = (int*) calloc(world_size, sizeof(int));
+		
         for (int i = 1; i < world_size; i++) {
             trows[i] = num_toss;
 
             if (i == world_size - 1)
-                trows[i] += tosses % (world_size - 1);
+                trows[i] += tosses % world_size - 1;
 
             MPI_Send(&trows[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
-    }
 
+		unsigned int seed = time(NULL);
+
+		double x, y;
+		for (int i = 0; i < num_toss; i++) {
+			x = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
+			y = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
+
+			if (x * x + y * y - rand_max <= eps) ans_sum += 1;
+		}	
+    }
+	
     if (world_rank == 0) {
         // TODO: process PI result
-        int ans = 0, tmp;
+        int tmp;
 
         for (int i = 1; i < world_size; i++) {
             MPI_Recv(&tmp, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            ans += tmp;
+            ans_sum += tmp;
+
+			// printf("%d %d\n", i, tmp);
         }
-        pi_result = (double) ans / tosses;
+        pi_result = (double) 4.0 * ans_sum / tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
