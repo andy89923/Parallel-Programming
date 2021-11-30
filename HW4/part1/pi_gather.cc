01a6154236fd87e6036876a61695326522ebc5e5
CTFang's Macbook <andy89923@gmail.com>
2021-11-30 20:33:41 +0800

    HW4 part1

--- HW4/part1/pi_gather.cc
+++ HW4/part1/pi_gather.cc
@@ -4,9 +4,24 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+typedef long long lol;
+#define eps 1e-9
 
-int main(int argc, char **argv)
-{
+const double rand_min = -1.0;
+const double rand_max =  1.0;
+
+void rand_toss(lol& ans, lol num_toss, unsigned int seed) {
+    double x, y;
+    for (lol i = 0; i < num_toss; i++) {
+        x = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
+        y = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
+
+        if (x * x + y * y - rand_max <= eps) *ans += 1;
+    }
+}
+
+
+int main(int argc, char **argv) {
     // --- DON'T TOUCH ---
     MPI_Init(&argc, &argv);
     double start_time = MPI_Wtime();
@@ -16,12 +31,31 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+
+    lol num_toss = tosses / world_size;
+    if (world_rank == world_size - 1)
+        num_toss += tosses % world_size;
+
+    lol* globl_ans = (lol*) calloc(world_size, sizeof(lol));
+    lol* local_ans = (lol*) calloc(world_size, sizeof(lol));
+
+    unsigned int seed = time(NULL) ^ world_rank;
+    rand_toss(local_ans[world_rank], num_toss, seed);
 
     // TODO: use MPI_Gather
+    MPI_Gather(local_ans, world_size, MPI_LONG_LONG, globl_ans, world_size, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
 
-    if (world_rank == 0)
-    {
+
+    if (world_rank == 0) {
         // TODO: PI result
+        lol ans_sum = 0;
+        for (int i = 0; i < world_size; i++)
+            ans_sum += globl_ans[i];
+
+        pi_result = (double) 4.0 * ans_sum / tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
--- HW4/part1/pi_reduce.cc
+++ HW4/part1/pi_reduce.cc
@@ -4,9 +4,23 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+typedef long long lol;
+#define eps 1e-9
 
-int main(int argc, char **argv)
-{
+const double rand_min = -1.0;
+const double rand_max =  1.0;
+
+void rand_toss(lol* ans, lol num_toss, unsigned int seed) {
+    double x, y;
+    for (lol i = 0; i < num_toss; i++) {
+        x = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
+        y = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
+
+        if (x * x + y * y - rand_max <= eps) *ans += 1;
+    }
+}
+
+int main(int argc, char **argv) {
     // --- DON'T TOUCH ---
     MPI_Init(&argc, &argv);
     double start_time = MPI_Wtime();
@@ -16,12 +30,25 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+    lol num_toss = tosses / world_size;
+    if (world_rank == world_size - 1)
+        num_toss += tosses % world_size;
+
+    lol* local_ans = (lol*) calloc(world_size, sizeof(lol));
+
+    unsigned int seed = time(NULL) ^ world_rank;
+    rand_toss(local_ans[world_rank], num_toss, seed);
+
+    lol ans;
     // TODO: use MPI_Reduce
+    MPI_Reduce(local_ans, &ans, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    if (world_rank == 0)
-    {
+    if (world_rank == 0) {
         // TODO: PI result
+        pi_result = (double) 4.0 * ans / tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
