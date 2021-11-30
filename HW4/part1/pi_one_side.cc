a87879aed2d5ea1b37478e212d16b80e151710fb
CTFang's Macbook <andy89923@gmail.com>
2021-12-01 00:00:21 +0800

    HW4 part1 reconstruct

--- HW4/part1/pi_one_side.cc
+++ HW4/part1/pi_one_side.cc
@@ -4,9 +4,23 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+typedef long long lol;
+#define eps 1e-9
 
-int main(int argc, char **argv)
-{
+const double rand_min = -1.0;
+const double rand_max =  1.0;
+
+void rand_toss(lol& ans, lol num_toss, unsigned int seed) {
+    double x, y;
+    for (lol i = 0; i < num_toss; i++) {
+        x = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
+        y = (rand_max - rand_min) * rand_r(&seed) / (RAND_MAX + 1.0) + rand_min;
+
+        if (x * x + y * y - rand_max <= eps) ans += 1;
+    }
+}
+
+int main(int argc, char **argv) {
     // --- DON'T TOUCH ---
     MPI_Init(&argc, &argv);
     double start_time = MPI_Wtime();
@@ -18,21 +32,22 @@ int main(int argc, char **argv)
     MPI_Win win;
 
     // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    if (world_rank == 0)
-    {
+    if (world_rank == 0) {
         // Master
     }
-    else
-    {
+    else {
         // Workers
     }
 
     MPI_Win_free(&win);
 
-    if (world_rank == 0)
-    {
+    if (world_rank == 0) {
         // TODO: handle PI result
+
+
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
