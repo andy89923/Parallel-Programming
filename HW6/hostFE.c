9fc3447bf76d1c464ca5bc7c08d1dd3ba8156640
CTFang's Macbook <andy89923@gmail.com>
2021-12-26 19:53:22 +0800

    HW6 first try

--- HW6/hostFE.c
+++ HW6/hostFE.c
@@ -4,10 +4,44 @@
 #include "helper.h"
 
 void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
-            float *inputImage, float *outputImage, cl_device_id *device,
-            cl_context *context, cl_program *program)
-{
-    cl_int status;
-    int filterSize = filterWidth * filterWidth;
+            float *inputImage, float *outputImage, 
+            cl_device_id *device, cl_context *context, cl_program *program) {
 
+    cl_int status;
+    cl_command_queue myqueue;
+    cl_program myprog;
+    cl_kernel kernel;
+    cl_mem inp_dat, oup_dat, fil_dat;
+
+    kernel = clCreateKernel(*program, "myConvolution", &status);
+    myqueue = clCreateCommandQueue(*context, *device, 0, &status);
+
+    int filt_size = filterWidth * filterWidth * sizeof(float);
+    int data_size = imageHeight *  imageWidth * sizeof(float);
+    int half_fitr = filterWidth / 2;
+
+    inp_dat = clCreateBufer(*context,    CL_MEM_READ_ONLY, data_size, NULL, &status);
+    oup_dat = clCreateBufer(*context,   CL_MEM_WRITE_ONLY, data_size, NULL, &status);
+    fil_dat = clCreateBufer(*context, CL_MEM_USE_HOST_PTR, filr_size, NULL, &status);
+
+
+    // set Argument
+    // cl_int clSetKernelArg(kernel, arg_indx, arg_size, arg_value)
+    clSetKernelArg(kernel, 0, sizeof(cl_mem), inp_dat);
+    clSetKernelArg(kernel, 1, sizeof(cl_mem), oup_dat);
+    clSetKernelArg(kernel, 2, sizeof(cl_mem), fil_dat);
+
+    clSetKernelArg(kernel, 3, sizeof(int), imageHeight);
+    clSetKernelArg(kernel, 4, sizeof(int), imageWidth);
+    clSetKernelArg(kernel, 5, sizeof(int), half_fitr);
+    
+
+    // set local and global workgroup sizes
+    int grid_size = imageHeight * imageWidth / 4;
+    // size_t loc_ws[2] = { 1, 1 };
+    size_t glb_ws[2] = { grid_size, 1 };
+
+    clEnqueueNDRangeKernel(myqueue, kernel, 2, 0, NULL, glb_ws, 0, NULL, NULL);
+    clFinish(myqueue);
+    clEnqueueReadBuffer(myqueue, out_dat, CL_TRUE, 0, data_size, ptr, 0, NULL, NULL);
 }
--- HW6/kernel.cl
+++ HW6/kernel.cl
@@ -1,4 +1,33 @@
-__kernel void convolution() 
-{
-   
+__kernel void convolution(
+   	const __global float* inp_dat, __global float4* oup_dat, __constant float* fil_dat,
+   	const int imageHeight, constant int imageWidth, constant int half_fitr) {
+
+	int gid = get_global_id(0);
+	int idx = gid / 4;
+
+	int poi_x = idx / imageWidth;
+	int poi_y = idx % imageWidth;
+
+	float4 ans = 0.0;
+
+	int filter_idx = 0;
+	for (int i = -half_fitr; i <= half_fitr; ++i) {
+
+		int now_x = poi_x + i;
+		if (now_x < 0 || now_x > imageHeight) continue;
+
+		for (int j = -half_fitr; j <= half_fitr; ++j, ++filter_idx) {
+
+			int now_y = poi_y + j;
+			if (now_y < 0 || now_y > imageWidth) continue;
+
+			int poi = now_y + now_x * imageWidth;
+			
+			float4 tmp = (float4)(inp_dat[poi], inp_dat[poi+1], inp_dat[poi+2], inp_dat[poi+3]);
+			float4 fil = fil_dat[filter_idx];
+
+			ans += tmp * fil;
+		}
+	}
+	oup_dat[gid] = ans;
 }
